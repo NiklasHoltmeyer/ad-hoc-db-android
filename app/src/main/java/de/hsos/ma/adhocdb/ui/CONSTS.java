ed7c0f80292441e94cd2ff6c0c0c31b828a7c51f
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-01-24 18:19:33 +0100

    colour

--- app/src/main/java/de/hsos/ma/adhocdb/ui/CONSTS.java
+++
@@ -1,8 +0,0 @@
-package de.hsos.ma.adhocdb.ui;
-
-public class CONSTS {
-    public static String itemId = "item.id";
-    public static String itemName = "item.name";
-    public static String itemImage = "item.image";
-    public static String itemDescription = "item.description";
-}
