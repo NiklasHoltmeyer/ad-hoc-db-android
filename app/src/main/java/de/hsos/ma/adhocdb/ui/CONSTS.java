ccaff0ca3af576f7027699062251f9a8072fe049
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-01-25 12:27:45 +0100

    consts

---
+++ app/src/main/java/de/hsos/ma/adhocdb/ui/CONSTS.java
@@ -0,0 +1,14 @@
+package de.hsos.ma.adhocdb.ui;
+
+public final class CONSTS {
+    public static final String itemId = "item.id";
+    public static final String itemName = "item.name";
+    public static final String itemImage = "item.image";
+    public static final String itemDescription = "item.description";
+
+    public static final String columnCount = "columnCount";
+
+    public static final String tableName = "tableName";
+    public static final String tableDescription = "tableDescription";
+    public static final String imageURL = "imageURL";
+}
