16282aef73df1821fe11c34e2caf522415a0ccb5
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-02-09 21:04:04 +0100

    images

--- app/src/main/java/de/hsos/ma/adhocdb/ui/images/show/ImageShowActivity.kt
+++ app/src/main/java/de/hsos/ma/adhocdb/ui/images/show/ImageShowActivity.kt
@@ -1,14 +1,18 @@
 package de.hsos.ma.adhocdb.ui.images.show
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.chrisbanes.photoview.PhotoView
 import de.hsos.ma.adhocdb.R
 import de.hsos.ma.adhocdb.entities.image.Image
 import de.hsos.ma.adhocdb.framework.persistence.database.ImagesDatabase
 import de.hsos.ma.adhocdb.framework.persistence.database.TablesDatabase
 import de.hsos.ma.adhocdb.ui.BaseCoroutineBaseMenuAppCompactActivity
 import de.hsos.ma.adhocdb.ui.INTENTCONSTS
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
 
 class ImageShowActivity : BaseCoroutineBaseMenuAppCompactActivity(
     R.layout.activity_image_show,
@@ -31,11 +35,19 @@ class ImageShowActivity : BaseCoroutineBaseMenuAppCompactActivity(
             val db = ImagesDatabase(applicationContext).imageDao()
             val image = db.getImageById(id.toString())
 
-            initImageView(image)
+            launch(Dispatchers.Main){
+                initImageView(image)
+            }
         }
     }
 
     private fun initImageView(image: Image) {
-        //findViewById<PhotoView>(R.id.photo_view)
+        val photoView = findViewById<PhotoView>(R.id.photo_view)
+
+        if (image.absolutePath.contains("media")){
+            photoView.setImageURI(Uri.fromFile(File(image.absolutePath)))
+        }else{
+            photoView.setImageResource(image.absolutePath.toInt())
+        }
     }
 }
